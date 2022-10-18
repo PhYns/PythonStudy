15b091404846ec13b21aaabaf54504099cef2660
Charles Martins <phyns.br@gmail.com>
2022-10-18 14:19:17 -0300

    added interview

--- Codewars/Removing Elements.py
+++ Codewars/Removing Elements.py
@@ -5,17 +5,21 @@
 
 # None of the arrays will be empty, so you don't have to worry about that!
 from unittest import TestCase as test
-my_list = ["Um","Dois","Tres"]
+
+
+my_list = [False, False, True, True]
 
 def remove_every_other(my_list):
-     elementsToRemove = [my_list[i] for i in range(1, len(my_list), 2)]
-     for element in elementsToRemove: my_list.remove(element)
-     return(my_list)
+    final_list = []
+    for index in range(len(my_list)):
+        if index % 2 == 0:
+            final_list.append(my_list[index])
+    return final_list
 
-remove_every_other(my_list)
-print(my_list)
 
-test.assertEquals(remove_every_other(first=['Hello', 'Goodbye', 'Hello Again']),second=['Hello', 'Hello Again'])
-test.assertEquals(remove_every_other(first=[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]),second=[1, 3, 5, 7, 9])
-test.assertEquals(remove_every_other(first=[[1, 2]]), second=[[1, 2]])
-test.assertEquals(remove_every_other(first=[['Goodbye'], {'Great': 'Job'}]), second=[['Goodbye']])
+print(remove_every_other(my_list))
+
+# test.assertEquals(remove_every_other(first=['Hello', 'Goodbye', 'Hello Again']), second=['Hello', 'Hello Again'])
+# test.assertEquals(remove_every_other(first=[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]), second=[1, 3, 5, 7, 9])
+# test.assertEquals(remove_every_other(first=[[1, 2]]), second=[[1, 2]])
+# test.assertEquals(remove_every_other(first=[['Goodbye'], {'Great': 'Job'}]), second=[['Goodbye']])
