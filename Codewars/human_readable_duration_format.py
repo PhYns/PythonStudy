de9d14c0728b5a66f2d40fdc94af91e6934f3ba1
Charles Martins <phyns.br@gmail.com>
2024-03-16 12:07:54 -0300

    fix: human readable with dict

--- Codewars/human_readable_duration_format.py
+++ Codewars/human_readable_duration_format.py
@@ -1,10 +1,13 @@
 def format_duration(seconds):
     if seconds == 0:
-        return now
+        return "now"
     else:
-        sec = seconds % 60
-        min = seconds % 3600
-        hou = seconds % 86400
-        day = seconds % 2592000
-        mon = seconds % 31104000
-        yea = seconds / 31104000
+
+        duration: dict = {'seconds': seconds % 60, 'minutes': seconds % 3600, 'hours': seconds % 86400,
+                          'days': seconds % 2592000, 'months': seconds % 31104000, 'years': 0}
+        if seconds / 31104000 > 1:
+            duration['years'] = int(seconds / 31104000)
+        return duration
+
+
+print(format_duration(140063843))
